ef647fd32ebeccb8213fd9bbfec51dc4d13f6025
veerakumar <veeravirat1881@gmail.com>
2026-02-11 17:15:20 +0530

    Alert: Added allert message before item gets deleted.

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,7 +3,7 @@ import ProductCard from "./ProductCard";
 export default function ProductList({ products, onEdit, onDelete }) {
   return (
     <div className="row g-4">
-      {products.map(p => (
+      {products.length !== 0 ? products.map(p => (
         <div key={p.id} className="col-md-4">
           <ProductCard
             product={p}
@@ -11,7 +11,16 @@ export default function ProductList({ products, onEdit, onDelete }) {
             onDelete={onDelete}
           />
         </div>
-      ))}
+      )) : <div style={{ textAlign: "center", marginBottom: 15 }}>
+        <span
+          style={{
+            color: "Grey",
+            padding: "8px 16px",
+          }}
+        >
+          Product match not found.
+        </span>
+      </div>}
     </div>
   );
 }
